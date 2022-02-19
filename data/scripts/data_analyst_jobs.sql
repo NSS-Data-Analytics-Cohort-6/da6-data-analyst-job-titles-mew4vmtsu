c5f0658a41f2bd011c2977fbbcff28dd30606a26
Michael Weston <mew4vmtsu@gmail.com>
2022-02-19 10:56:17 -0600

    through 12

--- data/scripts/data_analyst_jobs.sql
+++ data/scripts/data_analyst_jobs.sql
@@ -30,13 +30,53 @@ From data_analyst_jobs
 Where review_count
 Between 500 AND 1000;
 
-/* 6.Show the average star rating for companies in each state. The output should show the state as `state` and the average rating for the state as `avg_rating`. Which state shows the highest average rating?*/
+/* 6.Show the average star rating for companies in each state. The output should show the state as `state` and the average rating for the state as `avg_rating`. Which state shows the highest average rating? NE */
 
-Select *
+Select AVG(star_rating) AS avg_rating, location
 From data_analyst_jobs
-Where;
+Group by location
+Order By avg_rating DESC;
 
-/* 7.Select unique job titles from the data_analyst_jobs table. How many are there? */
+/* 7.Select unique job titles from the data_analyst_jobs table. How many are there? 881 */
+
+Select Count (Distinct title)
+From data_analyst_jobs;
+
+/* 8. How many unique job titles are there for California companies? 230 */
+
+Select Count (Distinct title)
+From data_analyst_jobs
+Where location ='CA';
+
+/* 9. Find the name of each company and its average star rating for all companies that have more than 5000 reviews across all locations. How many companies are there with more that 5000 reviews across all locations? 40 */ 
+
+Select AVG(star_rating) AS avg_rating, company
+From data_analyst_jobs
+Where review_count >= 5000
+Group By company;
+
+/* 10.	Add the code to order the query in #9 from highest to lowest average star rating. Which company with more than 5000 reviews across all locations in the dataset has the highest star rating? What is that rating?*/
+
+Select AVG(star_rating) AS avg_rating, company
+From data_analyst_jobs
+Where review_count >= 5000
+Group By company
+Order by avg_rating DESC;
+
+/* 11. Find all the job titles that contain the word ‘Analyst’. How many different job titles are there? 754 */
+
+Select Count (Distinct title)
+From data_analyst_jobs
+Where title Like '%Analyst%';
+
+/* 12. How many different job titles do not contain either the word ‘Analyst’ or the word ‘Analytics’? What word do these positions have in common? 26 */
+
+Select Count (Distinct title)
+From data_analyst_jobs
+Where (title) Not Like '%Analyst%'
+AND (title) Not like '%Analytics%';
+
+
 
 
 
